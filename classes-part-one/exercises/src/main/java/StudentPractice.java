27d43bea7b4031ab5fc218638e27326ca56460e7
Jake Willman <jakenwillman@gmail.com>
2023-08-26 17:30:25 -0500

    Started on Classes and Objects exercise. Need to fix step 2 and continue from there.

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,5 @@
 public class StudentPractice {
     public static void main(String[] args){
-        //insantiate your Student class below
+        Student student = new Student("Jake Willman", 12345, 1, 4.0); //insantiate your Student class below
     }
 }
