a3bfd1ec66799c451b3f2d8fc2e7a2d1c323806c
Jake Willman <jakenwillman@gmail.com>
2023-08-27 11:40:26 -0500

    completed Classes and Objects Exercise

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,14 @@
+
+
 public class StudentPractice {
+    //instantiate your Student class below
     public static void main(String[] args){
-        Student student = new Student("Jake Willman", 12345, 1, 4.0); //insantiate your Student class below
+        Student myself = new Student("Jake Willman", 12345, 1, 4.0);
+
+        // Print out the student's information
+        System.out.println("Student Name: " + myself.getName());
+        System.out.println("Student ID: " + myself.getStudentId());
+        System.out.println("Number of Credits: " + myself.getNumberOfCredits());
+        System.out.println("GPA: " + myself.getGpa());
     }
 }
